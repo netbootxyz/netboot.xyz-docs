b22e0835712a64310faa3169a212241941932b92
Antony Messerli <antony@mes.ser.li>
2021-08-10 22:46:27 -0500

    Tweaks to index files

---
+++ src/components/HomepageFeatures.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+  {
+    title: 'Simple to Use',
+    Svg: require('../../static/img/nbxyz-logo.svg').default,
+    description: (
+      <>
+        netboot.xyz enables you to boot into many types of operating systems using lightweight tooling to get you up and 
+        running as soon as possible.
+      </>
+    ),
+  },
+  {
+    title: 'Evaluate, Install, Rescue',
+    Svg: require('../../static/img/nbxyz-logo.svg').default,
+    description: (
+      <>
+        Discover new operating systems without having to download and rewrite media over and over again.  Rescue operating
+	systems from a single image. An essential for any sysadmin.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by the iPXE project',
+    Svg: require('../../static/img/ipxe-logo.svg').default,
+    description: (
+      <>
+        netboot.xyz uses the iPXE project to enable you to provision, rescue or load into a live boot environment leveraging the 
+        the Preboot Execution Environment (PXE) on most systems.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,95 +3,38 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
 
-const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/nbxyz-logo.svg',
-    description: (
-      <>
-        netboot.xyz enables you to boot into many types of operating systems using lightweight tooling to get you up and 
-        running as soon as possible.
-      </>
-    ),
-  },
-  {
-    title: 'Evaluate, Install, Rescue',
-    imageUrl: 'img/nbxyz-logo.svg',
-    description: (
-      <>
-        Discover new operating systems without having to download and rewrite media over and over again.  Rescue operating
-	systems with a single tool. An essential for any sysadmin.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by the iPXE project',
-    imageUrl: 'img/ipxe-logo.svg',
-    description: (
-      <>
-        netboot.xyz uses the iPXE project to enable you to provision, rescue or load into a live boot environment leveraging the 
-        the Preboot Execution Environment (PXE) on most systems.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs">
+            Get Started
+          </Link>
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
+      </div>
+    </header>
   );
 }
 
-function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Your favorite operating systems in one place!`}
       description="netboot.xyz enables you to PXE boot Operating System installers and utilities from a simple to use menu based on the iPXE project.">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
+      <HomepageHeader />
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+        <HomepageFeatures />
       </main>
     </Layout>
   );
 }
-
-export default Home;
