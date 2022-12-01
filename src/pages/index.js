f20923f262e4ca69e4b7a4433d957a57634d4918
Antony Messerli <antony@mes.ser.li>
2022-11-30 22:31:09 -0600

    Update front page logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <img src="img/nbxyz-transparent-logo-white.png"></img>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
