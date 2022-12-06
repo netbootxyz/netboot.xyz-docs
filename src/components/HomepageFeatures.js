dc988c891a769962fff6c896cd1939ec9495dd91
Antony Messerli <antony@mes.ser.li>
2022-12-06 00:43:15 -0600

    Update iPXE image

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -28,7 +28,7 @@ const FeatureList = [
   },
   {
     title: "Powered by the iPXE project",
-    Svg: ["./img/ipxe-logo.svg", "./img/ipxe-logo.svg"],
+    Svg: ["./img/ipxechip.svg", "./img/ipxechip.svg"],
     description: (
       <>
         netboot.xyz uses the iPXE project to enable you to provision, rescue or
