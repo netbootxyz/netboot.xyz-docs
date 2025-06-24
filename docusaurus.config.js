8f049bf0206b08d569ba354b72d7928bc45011d0
Antony Messerli <antony@mes.ser.li>
2025-06-24 01:23:10 -0500

    SEO updates, update X logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
-const TwitterSvg =
-  '<svg style="fill: #1DA1F2; vertical-align: middle;" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>';
+const XSvg =
+  '<svg style="fill: #000000; vertical-align: middle;" width="14" height="14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></svg>';
 
 module.exports = {
   title: "netboot.xyz",
@@ -16,9 +16,17 @@ module.exports = {
   },
   themes: ['@docusaurus/theme-mermaid'],
   themeConfig: {
-    metadata: [{name: 'keywords', content: 'ipxe, netbootxyz, pxe, linux, os, operating system, install, installer, netboot, netboot.xyz'}],
+    metadata: [
+      {name: 'keywords', content: 'ipxe, netbootxyz, pxe, linux, os, operating system, install, installer, netboot, netboot.xyz, network boot, tftp, uefi'},
+      {name: 'description', content: 'Network boot your favorite operating systems, installers and utilities from one menu over the network with iPXE'},
+      {name: 'author', content: 'netboot.xyz team'},
+      {property: 'og:type', content: 'website'},
+      {property: 'og:image', content: 'https://netboot.xyz/img/nbxyz_logo_name.png'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'twitter:site', content: '@netbootxyz'}
+    ],
     prism: {
-      additionalLanguages: ['bash', 'yaml'],
+      additionalLanguages: ['bash', 'yaml', 'json', 'nginx', 'ini', 'powershell', 'python'],
     },
     docs: {
       sidebar: {
@@ -32,7 +40,7 @@ module.exports = {
     },
     announcementBar: {
       id: "announcementBar-1", // Increment on change
-      content: `If you like netboot.xyz, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/netbootxyz/netboot.xyz">GitHub</a>⭐️, follow us on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/netbootxyz" >Twitter</a> ${TwitterSvg} and join our <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/An6PA2a">Discord</a>!`,
+      content: `If you like netboot.xyz, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/netbootxyz/netboot.xyz">GitHub</a>⭐️, follow us on <a target="_blank" rel="noopener noreferrer" href="https://x.com/netbootxyz">${XSvg}</a> and join our <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/An6PA2a">Discord</a>!`,
     },
     colorMode: {
       defaultMode: "light",
@@ -42,6 +50,11 @@ module.exports = {
       appId: "BMY28LDVW4",
       apiKey: "51b51a157c47742003b8943f2c5acc09",
       indexName: "netboot",
+      contextualSearch: true,
+      searchParameters: {
+        facetFilters: [],
+      },
+      searchPagePath: 'search',
     },
     navbar: {
       title: "netboot.xyz",
